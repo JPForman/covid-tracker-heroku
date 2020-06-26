ce800d3aaa3e80107be5e5046eebce7cd91f618a
Joshua Forman <cyborgforman@gmail.com>
2020-06-26 11:35:39 -0700

    add covid src from non heroku compatable repo

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api';
+
+import coronaImage from './images/image.png';
+
+import styles from './App.module.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+      country: '',
+    }
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    
+    this.setState({ data: fetchedData });
+  }
+  
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    
+    this.setState({ data: fetchedData, country: country });
+  } 
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt='corona image'/>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country}/>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {HashRouter} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <HashRouter>
-   <App />
-  </HashRouter>,
-   document.getElementById('root'));
+    <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
